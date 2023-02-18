3d6b25375ea759849502aadd9d3cbd3a2e603c76
Nemanja Arsic <nemanja.arsic815@gmail.com>
2023-02-18 16:16:32 +0100

    Implemented sudoku validator

--- src/validator.js
+++ src/validator.js
@@ -1,3 +1,5 @@
+const ValidationFunctions = require('./validationFunctions')
+
 class Validator {
   static validate(sudoku) {
     const validator = new Validator
@@ -6,7 +8,37 @@ class Validator {
   }
 
   validate(sudoku) {
-    // Your code here
+    const vf = new ValidationFunctions()
+
+    let nums = vf.getNumbers(sudoku)
+    if (nums.length != 81) {
+      return vf.invalidMsg
+    }
+    
+    let rows = vf.getRows(nums)
+    for(let i = 0; i < 9; i++){
+      if(vf.lookForDuplicates(rows[i])){
+        return vf.invalidMsg
+      }
+    }
+
+    let columns = vf.getColumns(rows)
+    for(let i = 0; i < 9; i++){
+      if(vf.lookForDuplicates(columns[i])){
+        return vf.invalidMsg
+      }
+    }
+
+    let subgroups = vf.createSubgroups(rows)
+    for(let i = 0; i < 9; i++){
+      if(vf.lookForDuplicates(subgroups[i])){
+        return vf.invalidMsg
+      }
+    }
+
+    let hasZero = vf.lookForZero(nums)
+
+    return hasZero ? vf.incompleteMsg : vf.validMsg
   }
 }
 
